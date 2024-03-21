9101612e7421c74a9b2874f6065265f84ea4769d
Jirayu Kaewsing <jirayukaewsing48@gmail.com>
2024-03-21 11:37:48 +0700

    Generate base template with cobra-cli

---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2024 Jirayu Kaewsing strixz.self@gmail.com, kernel137 kostamecev@pm.me
+
+*/
+package main
+
+import "github.com/StrixzIV/shavis-go/cmd"
+
+func main() {
+	cmd.Execute()
+}
