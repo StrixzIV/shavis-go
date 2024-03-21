50d348647e93089f4bfa1678b13f913494408bab
Jirayu Kaewsing <strixz.self@gmail.com>
2024-03-21 13:30:10 +0700

    Adding git flags

--- cmd/root.go
+++ cmd/root.go
@@ -1,12 +1,15 @@
 /*
 Copyright © 2024 Jirayu Kaewsing strixz.self@gmail.com, kernel137 kostamecev@pm.me
-
 */
 package cmd
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 
+	"github.com/go-git/go-git/v5"
 	"github.com/spf13/cobra"
 )
 
@@ -22,7 +25,7 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: run,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -43,5 +46,26 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().BoolP("git-latest", "l", false, "Use a latest git commit hash to generate 8x5 image")
+	rootCmd.PersistentFlags().StringP("git", "g", "", "Use a specified git commit hash to generate 8x5 image")
+}
+
+func run(cmd *cobra.Command, args []string) {
+
+	git_hash, _ := cmd.Flags().GetString("git")
+	use_latest, _ := cmd.Flags().GetBool("git-latest")
+
+	if use_latest {
+
+		executable, _ := os.Executable()
+
+		repo, _ := git.PlainOpen(filepath.Dir(executable))
+		ref, _ := repo.Head()
+
+		git_hash = strings.Split(ref.String(), " ")[0]
+
+	}
+
+	fmt.Println(git_hash)
+
 }
