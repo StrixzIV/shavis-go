f09bd5afda2e81ba5e8c98828b28322b36d57c24
Jirayu Kaewsing <strixz.self@gmail.com>
2024-03-21 14:32:46 +0700

    Update command description

--- cmd/root.go
+++ cmd/root.go
@@ -19,15 +19,10 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "shavis-go",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
+	Short: "A Go implimentation of SHA256 or SHA1 hash sum visualization, either directly, file or git commit hash based on: https://github.com/kernel137/shavis",
+	Long: `A Go implimentation of SHA256 or SHA1 hash sum visualization, either directly, file or git commit hash
+based on https://github.com/kernel137/shavis original implimentation in Python
+	`,
 	Run: run,
 }
 
@@ -48,6 +43,7 @@ func init() {
 
 	rootCmd.PersistentFlags().BoolP("git-latest", "l", false, "Use a latest git commit from current working directory hash to generate 8x5 image")
 	rootCmd.PersistentFlags().StringP("git", "g", "", "Use a specified git commit hash to generate 8x5 image")
+
 }
 
 // initConfig reads in config file and ENV variables if set.
