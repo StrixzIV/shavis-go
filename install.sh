aa127628247c7b20fa902b260539d0dad8a9c351
Jirayu Kaewsing <jirayukaewsing48@gmail.com>
2024-03-27 02:11:10 +0700

    Add config file check condition for UNIX setup script

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 go install 
 
+if [ ! -e ~/.shavis-go.yaml ]; then
+echo "Copying config file..."
 cp .shavis-go.yaml ~/
+fi
 
 echo "Installation complete!"
 echo "shavis-go config are stored in $HOME/.shavis-go.yaml"
